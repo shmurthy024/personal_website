176d9315b4b2faec9c6a3e2e408942f63f89639e
Shree Murthy <67122293+shmurthy024@users.noreply.github.com>
2021-07-06 20:00:27 -0700

    deployment 1

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/umd/entry.webpack';
-import pdf from "../PDFs/Shree_Murthy_Resume.pdf"
 import './Resume.css';
 function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-  return (
+   return (
     <div className="resume-div">
-      <Document
-        file={pdf}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber} />
-      </Document>
-      
+      <h1> Who am I? </h1>
+      <ul>
+        <li> I was born on July 4th, 2002 in Las Vegas, NV </li>
+        <li> My favorite sport is basketball and my favorite team is the Los Angeles lakers</li>
+        <li> I love to hang out with friends and talk about our interests</li>
+        <li> I love to read and watch tv </li>
+        <ul>
+          <li> My favorite book is Percy Jackson and my favorite show is Loki </li>
+        </ul>
+        <li> My biggest desire in life is to help as many people as possible succeed and find their best selves </li>
+
+      </ul>
+  
+      <h2> Upcoming Project/Certification ideas: </h2>
+      <ul>
+        <li> An Email Automation App: Easily send updates to my supervisors at my Chapman Grand Office Assistant Position</li>
+        <li> Logging in with Spotify: An app to teach myself how to leverage Spotify's API to log people into an app</li>
+        <li> Getting the AWS Practioner Certificate: I love Cloud Computing and it's my dream to work in this field </li>
+      </ul>
     </div>
   );
 }
